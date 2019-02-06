d80fe3c456deeca4433045ba2497560eb7054194
elisamartin <elisamv93@gmail.com>
2019-02-06 23:56:35 +0100

    Nav animation almost done

--- index.js
+++ index.js
@@ -1,4 +1,36 @@
 // Nav animation
+/*
+$('.h-menu').on('click', function() {
+	$(this).toggleClass('active');
+});
+*/
+
+class Menu {
+	constructor(element) {
+		this.element = element;
+		this.button = document.querySelector('.h-menu');
+		this.content = document.querySelector('.top-nav');
+		this.links = document.querySelectorAll('.top-nav a');
+
+		this.button.addEventListener('click', () => {
+			this.toggleContent();
+			this.toggleCross();
+		});
+	}
+	toggleContent() {
+		this.content.classList.toggle('hide');
+		this.content.classList.toggle('show');
+		this.links.forEach((link) => {
+			link.classList.toggle('hide');
+			link.classList.toggle('show');
+		});
+	}
+	toggleCross() {
+		this.button.classList.toggle('active');
+	}
+}
+
+let menus = document.querySelectorAll('header').forEach((menu) => new Menu(menu));
 
 // FAQ counter
 
